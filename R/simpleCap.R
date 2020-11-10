cf9e247ac07322a1c0ccd18f8bb66a65655a4f66
sieu-tran <sieu.tran@accenturefederal.com>
2020-11-10 12:08:13 -0500

    testing

--- R/simpleCap.R
+++
@@ -1,7 +0,0 @@
-#' Capitalize the first letter of every word in a string
-#'
-simpleCap <- function(x) {
-  s <- strsplit(x, " ")[[1]]
-  paste(toupper(substring(s, 1,1)), substring(s, 2),
-        sep="", collapse=" ")
-}
