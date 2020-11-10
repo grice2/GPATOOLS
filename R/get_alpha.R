58bece855064c0cfbea572fea67ef549289aa71f
gretta <42980019+otalinda@users.noreply.github.com>
2020-11-10 12:21:19 -0500

    Fix get_alpha function.

--- R/get_alpha.R
+++ R/get_alpha.R
@@ -1,10 +1,13 @@
 #' This function generates the p-values associated with
-#' several treatments against a control, for a single survey question. 
+#' several treatments against a SINGLE control, for a single survey question. 
 #' 
+#' #Preprocessing that generates input DF usually looks like this:
+#' Q4.ad.analysis <- combined.data %>% group_by(Ad,Q4) %>% summarise(n_q4=n()) %>% group_by(Ad) %>% 
+#' mutate(total_q4 = sum(n_q4)) %>% mutate(percent_q4 = n_q4/total_q4) %>% filter(Q4 == 1)
 #' 
 #' Required Parameters:
 #'
-#' df - df containing survey response data
+#' df - df containing survey response data, summarized at the treatment level and containing counts of responses
 #' treatment - column specifying treatments of interest
 #' counts - column containing response counts 
 #' size - column containing sample size for each treatment 
@@ -26,7 +29,6 @@ get_alpha <- function(df, treatment, counts, size, alt){
                 alternative = alt)$p.value),4)
     print(i)
    }
-  pvals<- pvals[-length(pvals)]
   return(pvals)
   }
 
