e1ceeaa9ace6a53bf6e84877ddcd236a429ba727
sieu-tran <sieu.tran@accenturefederal.com>
2021-02-25 12:05:09 -0500

    Update read_all.R

--- R/read_all.R
+++ R/read_all.R
@@ -1,14 +1,14 @@
 #' Read All Data in the Directory
 #'
 #' This function allows user to read and combine all like-data with the extension .csv, .xlsx, or .rds in a folder.
-#' 
+#'
 #' Developer note: read_all() is essectially useful when reading Pollfish data for a persuasion test.
 #'
 #' @param dir The directory where all of your data are located
 #' @param sheet The name of the excel sheet where your data are located
 #' @return A combined data frame of all data in the directory or a list of combined like-datasets
 #' @examples df<- read_all("C:/Users/", "Individuals Coded")
-#' 
+#'
 #' @export
 read_all <- function(dir, sheet = "Individuals Coded"){
   file_list <- list.files(dir, pattern=c("*.csv|*.xlsx|*.rds"), full.names=TRUE)
@@ -23,30 +23,30 @@ read_all <- function(dir, sheet = "Individuals Coded"){
     temp <- try(readxl::read_excel(file_list[i], sheet = sheet))
     if (class(temp) == "try-error") {
       temp <-data.table::fread(file_list[i])
-    } 
+    }
     temp$survey <- basename(file_list[i])
     if (prod(colnames(df) == colnames(temp)) == 1){
-      df <- rbindlist(list(df, temp), use.names = T) 
+      df <- rbindlist(list(df, temp), use.names = T)
       df_list[[j]] <- df
-    }  
+    }
     else {
       k <- 1
       need_to_append <- TRUE
       while (k <= j) {
         if(prod(colnames(temp) == colnames(df_list[[k]])) == 1){
-          df_list[[k]] <- rbindlist(list(df_list[[k]], temp), use.names = T) 
+          df_list[[k]] <- rbindlist(list(df_list[[k]], temp), use.names = T)
           need_to_append <- FALSE
         }
         k <- k+1
       }
       if (k>j & need_to_append) {
         df_list[[k]] <- temp
-        j <- k
+        j <- j+1
       }
     }
   }
-  
-  
+
+
   if (length(df_list)>1) {
     return(df_list)
   } else {
